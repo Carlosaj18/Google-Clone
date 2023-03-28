301fa8cbe600b0d93cdb29286035008b8ed86326
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-03-28 01:19:05 -0500

    Building the Login Functionality

--- src/scenes/LoginPage/Login.js
+++ src/scenes/LoginPage/Login.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import { Button } from "@mui/material";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../features/userSlice";
+import { auth, provider } from "../../firebase";
+import "./Login.css";
+import { signInWithPopup } from "firebase/auth";
 
 function Login() {
+  const dispatch = useDispatch();
+  const signIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      dispatch(
+        login({
+          displayName: user.displayName,
+          email: user.email,
+          photoUrl: user.photoURL,
+        })
+      );
+    } catch (error) {
+      console.error("Error signing in with Google", error);
+    }
+  };
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <div className="login_container">
+        <img
+          src="https://static.dezeen.com/uploads/2020/10/gmail-google-logo-rebrand-workspace-design_dezeen_2364_col_0.jpg"
+          alt="Gmail Login"
+        />
+        <Button onClick={signIn}>Login</Button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
