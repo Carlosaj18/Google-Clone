de5107ce1e1a44b50d9dbb096a617b3c3b792180
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-03-28 01:20:20 -0500

    Refining the Build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import HomePage from "./scenes/homePage/homePage";
+import React, { useEffect } from "react";
 import LoginPage from "./scenes/LoginPage/Login";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./scenes/global/Header/Header";
+import Sidebar from "./scenes/global/Sidebar/Sidebar";
 import "./App.css";
+import Mail from "./scenes/Mail/Mail";
+import EmailList from "./scenes/EmailList/EmailList";
+import { useDispatch, useSelector } from "react-redux";
+import { selectSendMessageIsOpen } from "./features/mailSlice";
+import SendMail from "./components/SendMail/SendMail";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
+import { login, selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function App() {
-  
-  let user = true; 
+  const user = useSelector(selectUser);
+  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        // the user is logged in
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        );
+      }
+    });
+  }, []);
 
   return (
-    <div className="app">
-      {!user ? (
-        <LoginPage />
-      ) : (
-        <HomePage />
-      )}
-    </div>
+    <Router>
+      <ErrorBoundary>
+        <div className="App">
+          {user ? (
+            <>
+              <Header />
+              <div className="app_body">
+                <Sidebar />
+                <Routes>
+                  <Route path="/mail" element={<Mail />} />
+                  <Route path="/" element={<EmailList />} />
+                </Routes>
+              </div>
+              {sendMessageIsOpen && <SendMail />}
+            </>
+          ) : (
+            <LoginPage />
+          )}
+        </div>
+      </ErrorBoundary>
+    </Router>
   );
 }
-
 export default App;
 
 /** @
